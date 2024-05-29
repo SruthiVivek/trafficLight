a7cb3e217fb3a61efdcb4b8b1d443bc5aff96566
unknown <https://github.com/SruthiVivek/trafficLight.git>
2024-05-29 11:37:49 +0530

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const redLight = document.getElementById('red');
+    const yellowLight = document.getElementById('yellow');
+    const greenLight = document.getElementById('green');
+
+    const lights = [redLight, yellowLight, greenLight];
+    const durations = [15000, 10000, 5000]; 
+
+    let currentLight = 0;
+
+    function changeLight() {
+        
+        lights.forEach(light => light.classList.remove('active'));  // Turn off all lights
+
+      
+        lights[currentLight].classList.add('active');      // Turn on the current light
+
+        
+        currentLight = (currentLight + 1) % lights.length;   // Move to the next light
+
+      
+        setTimeout(changeLight, durations[currentLight]);     // Set timeout for the next change
+    }
+
+
+    changeLight();       // Start the traffic light sequence
+});
